b4fd2e6415c25ef7097805d81c843eff27fbb993
Fedron <40535546+Fedron@users.noreply.github.com>
2020-03-10 14:10:32 +0000

    Moved Jobs to a DB
    Jobs are now in a JSON file from which jobs.html reads and application.html gets info

--- resources/js/application.js
+++ resources/js/application.js
@@ -1,32 +1,56 @@
-const urlParams = new URLSearchParams(window.location.search);
-document.querySelector("#job").innerHTML = `
-<div class="title">
-    ${urlParams.get("name")}
-</div>
-<div class="headers">
-    <div class="main-info">
-        <h1><i class="fas fa-coins"></i> ${urlParams.get("salary")}</h1>
-        <h1><i class="fas fa-clock"></i> ${urlParams.get("time")}</h1>
-        <h1><i class="fas fa-city"></i> ${urlParams.get("city")}</h1>
-    </div>
-    <div class="btn">View Info</div>
-</div>
-<div class="info hidden">
-    ${urlParams.get("info")}
-</div>
-`;
+async function loadJobInfo() {
+    let jobsJSON =  await axios.get("resources/js/jobs.json");
+    jobsJSON = jobsJSON.data;
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const job = jobsJSON.filter(j => j.id === parseInt(urlParams.get("id")))[0];
+    
+    if (!job) {
+        document.querySelector("form").classList.add("hidden");
+        document.querySelector("#job").classList.add("hidden");
 
-const jobInfo = document.querySelector("#job .info");
-const viewInfoBtn = document.querySelector("#job .btn");
-viewInfoBtn.addEventListener("click", () => {
-    if (jobInfo.classList.contains("hidden")) {
-        jobInfo.classList.remove("hidden");
-        viewInfoBtn.innerHTML = "Collapse Info";
-    } else {
-        jobInfo.classList.add("hidden");
-        viewInfoBtn.innerHTML = "View Info";
+        const alert = document.querySelector("#alert");
+        alert.style.backgroundColor = "#eb4034"
+        alert.querySelector("p").innerHTML = `Job ID not found`;
+
+        alert.classList.remove("hidden");
+
+        return;
     }
-});
+
+    document.querySelector("#job").innerHTML = `
+    <div class="title">
+        ${job.name}
+    </div>
+    <div class="headers">
+        <div class="main-info">
+            <h1><i class="fas fa-coins"></i> ${job.salary}</h1>
+            <h1><i class="fas fa-clock"></i> ${job.time}</h1>
+            <h1><i class="fas fa-city"></i> ${job.location}</h1>
+        </div>
+        <div class="btn">View Info</div>
+    </div>
+    <div class="info hidden">
+        ${job.mainInfo}
+        <br><br>
+        ${job.extraInfo}
+    </div>
+    `;
+
+    const jobInfo = document.querySelector("#job .info");
+    const viewInfoBtn = document.querySelector("#job .btn");
+    viewInfoBtn.addEventListener("click", () => {
+        if (jobInfo.classList.contains("hidden")) {
+            jobInfo.classList.remove("hidden");
+            viewInfoBtn.innerHTML = "Collapse Info";
+        } else {
+            jobInfo.classList.add("hidden");
+            viewInfoBtn.innerHTML = "View Info";
+        }
+    });
+}
+
+loadJobInfo();
 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
--- resources/js/jobs.js
+++ resources/js/jobs.js
@@ -1,30 +1,47 @@
-const jobs = document.querySelectorAll(".job");
-jobs.forEach((element) => {
-    const expandButton = element.querySelector(".expand");
-    const extraInfo = element.querySelector(".extra-info");
-
-    // Expand button functionality
-    expandButton.addEventListener("click", () => {
-        if (extraInfo.classList.contains("hidden")) {
-            extraInfo.classList.remove("hidden");
-            expandButton.innerHTML = "Collapse";
-        } else {
-            extraInfo.classList.add("hidden");
-            expandButton.innerHTML = "Expand";
-        }
+async function loadJobs() {
+    let jobsJSON =  await axios.get("resources/js/jobs.json");
+    jobsJSON = jobsJSON.data;
+    
+    const jobsContainer = document.querySelector(".job-list");
+    jobsJSON.forEach((job, index) => {
+        jobsContainer.innerHTML += `
+        <div class="job">
+            <div class="name">
+                <h1>${job.name}</h1>
+            </div>
+            <div class="desc">
+                <div class="headers">
+                    <div class="main-info">
+                        <h1><i class="fas fa-coins"></i> ${job.salary}</h1>
+                        <h1><i class="fas fa-clock"></i> ${job.time}</h1>
+                        <h1><i class="fas fa-city"></i> ${job.location}</h1>
+                    </div>
+                    <a class="btn" href="application.html?id=${job.id}">Apply Now</a>
+                </div>
+                <div class="info">${job.mainInfo}</div>
+                <div class="extra-info hidden">${job.extraInfo}</div>
+                <div class="expand btn">Expand</div>
+            </div>
+        </div>
+        `
     });
 
-    // Apply button functionality
-    const applyButton = element.querySelector(".btn");
-    const jobName = element.querySelector(".name").textContent.trim();
+    const jobs = document.querySelectorAll(".job");
+    jobs.forEach((element) => {
+        const expandButton = element.querySelector(".expand");
+        const extraInfo = element.querySelector(".extra-info");
 
-    const headers = element.querySelector(".main-info");
-    const salary = headers.childNodes[1].textContent.trim();
-    const time = headers.childNodes[3].textContent.trim();
-    const city = headers.childNodes[5].textContent.trim();
-    const info = element.querySelector(".info").textContent.trim();
+        // Expand button functionality
+        expandButton.addEventListener("click", () => {
+            if (extraInfo.classList.contains("hidden")) {
+                extraInfo.classList.remove("hidden");
+                expandButton.innerHTML = "Collapse";
+            } else {
+                extraInfo.classList.add("hidden");
+                expandButton.innerHTML = "Expand";
+            }
+        });
+    });
+}
 
-    applyButton.setAttribute("href", `
-        application.html?name=${jobName}&salary=${salary}&time=${time}&city=${city}&info=${info}
-    `);
-});
+loadJobs();
